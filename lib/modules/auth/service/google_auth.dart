e7d234da01262d242f41140d328b95c9eaa722d9
Pedro Jair Ramos Lima <pjairdev@gmail.com>
2025-11-28 05:48:32 -0300

    Update google_auth.dart

--- lib/modules/auth/service/google_auth.dart
+++ lib/modules/auth/service/google_auth.dart
@@ -10,7 +10,7 @@ class GoogleSignInService {
     if (!isInitialize) {
       await _googleSignIn.initialize(
         serverClientId:
-            'http://484988555302-d91nev5jn5sit0qoe3oehpgpp58pl5mt.apps.googleusercontent.com',
+            '569173351536-0i50cbpukr24kcv7ilmcjnccslrgka8r.apps.googleusercontent.com',
       );
     }
     isInitialize = true;
